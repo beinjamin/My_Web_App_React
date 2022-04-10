93fc01023766286b31fc715245b5dd8076e125d5
beinjamin <jauresbeinjamin@gmail.com>
2022-04-10 11:32:42 +0100

    Insertion des element class

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 class Counter extends Component {
     render() {
         return (
-            <div>
+            <div className="card">
+            <div className="card-header">Titre</div>
                 
             </div>
         );
