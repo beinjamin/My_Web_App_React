f85fe1a13713f670f848324802fbe763ad2d48de
beinjamin <jauresbeinjamin@gmail.com>
2022-04-10 12:01:01 +0100

    Mise en avant les proprieter react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./components/counter";
+
 
 function App() {
   return (
-  <div>
-    <h2>Bonjour... </h2>
+  <div className="m-3">
+    <Counter title="Counter 1"value={1} image="images/profile.png" />
   </div>
   );
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <div className="card">
-            <div className="card-header">Titre</div>
-                
+                <div className="card-header">
+                    {this.props.title}
+                </div>
+            <div className="card-body">
+                    Content
             </div>
+            </div>
+                
+        
         );
     }
 }
