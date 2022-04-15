cde6f5fb64a0d763fd4ffd0f1a913d7e9349e68c
beinjamin <jauresbeinjamin@gmail.com>
2022-04-15 22:32:53 +0100

    Mise en place du systeme de routage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/counter";
-import {Router, Switch,Link,BrowserRouter as Router} from 'react-router-dom';
+import {Route, Switch,Link,BrowserRouter as Router} from 'react-router-dom';
+import About from "./components/About";
 
 
 function App() {
@@ -31,9 +32,12 @@ function App() {
     </nav>
     <div className='container'>
     <Switch>
+    <Router path='/home'></Router>
+
     <Router path='/counter' component={Counter}></Router>
-    <Router path='/counter' component={Counter}></Router>
-    <Router path='/counter' component={Counter}></Router>
+    <Router path='/about' component={About}></Router>
+    <Router path='/gallery' ></Router>
+
 
 
     </Switch>
