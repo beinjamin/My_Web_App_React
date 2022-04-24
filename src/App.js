aa4f660aeb1c6b9b9abeaf0fbd2ac2932cafe5ef
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 13:34:24 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/counter";
-import {Route, Switch,Link,BrowserRouter as Router} from 'react-router-dom';
 import About from "./components/About";
+
+import {Route, Switch,Link,BrowserRouter as Router} from 'react-router-dom';
 
 
 function App() {
@@ -12,16 +13,16 @@ function App() {
     <nav className="navbar navbar-expand navbar-brand m-2">
     <ul className="navbar-nav">
       <li> 
-      <link className='nav-link' to="/home">Home</link> 
+      <Link className='nav-link' to="/home">Home</Link> 
       </li>
       <li> 
-      <link className='nav-link'  to="/counter">Counter</link> 
+      <Link className='nav-link'  to="/counter">Counter</Link> 
       </li>
       <li> 
-      <link className='nav-link'  to="/about">About</link> 
+      <Link className='nav-link'  to="/about">About</Link> 
       </li>
       <li> 
-      <link className='nav-link' to="/gallery">Gallery</link> 
+      <Link className='nav-link' to="/gallery">Gallery</Link> 
       </li>
     </ul>
     
@@ -32,11 +33,11 @@ function App() {
     </nav>
     <div className='container'>
     <Switch>
-    <Router path='/home'></Router>
+    <Route path='/home'></Route>
 
-    <Router path='/counter' component={Counter}></Router>
-    <Router path='/about' component={About}></Router>
-    <Router path='/gallery' ></Router>
+    <Route path='/counter' component={Counter}></Route>
+    <Route path='/about' component={About}></Route>
+    <Route path='/gallery' ></Route>
 
 
 
@@ -49,3 +50,4 @@ function App() {
 }
 
 export default App;
+
